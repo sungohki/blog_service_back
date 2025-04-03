bcb950b65439368c83b48fc33d836ac65c299c27
sungohki <sokim1201@naver.com>
2025-04-03 12:32:32 +0900

    chore: 디렉토리명 변경

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -2,7 +2,7 @@ import Router from 'koa-router';
 
 const postApi = new Router();
 
-postApi.get('/');
+postApi.get('/', (ctx) => {});
 postApi.post('/');
 postApi.get('/:id');
 postApi.post('/:id');
