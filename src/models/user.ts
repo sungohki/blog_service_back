70d0f73f6fef5c0c3a109a24c1780ed534ce66dc
sungohki <sokim1201@naver.com>
2025-04-03 18:24:25 +0900

    feat: User 모델 정의

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,24 +1,46 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose, { Model, Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
 
-const userSchema = new Schema({
-  username: String,
-  hashedPassword: String,
+interface IUser {
+  username: string;
+  hashedPassword?: string;
+}
+
+interface IUserDocument extends IUser, Document {
+  setPassword: (password: string) => Promise<void>;
+  checkPassword: (password: string) => Promise<boolean>;
+  serialize: () => IUser;
+}
+
+interface IUserModel extends Model<IUserDocument> {
+  findByUsername: (username: string) => Promise<IUserDocument>;
+}
+
+// 스키마 정의
+const userSchema: Schema<IUserDocument> = new Schema({
+  username: { type: String, required: true },
+  hashedPassword: { type: String },
 });
 
 // 인스턴스 메서드
-userSchema.methods.setPassword = async function (newPassword: string) {
-  this.hashedPassword = await bcrypt.hash(newPassword, 10);
+userSchema.methods.setPassword = async function (password: string) {
+  const hashed = await bcrypt.hash(password, 10);
+  this.hashedPassword = hashed;
 };
-userSchema.methods.checkPassword = async function (passwordToCheck: string) {
-  return bcrypt.compare(passwordToCheck, this.hashedPassword);
+userSchema.methods.checkPassword = async function (password: string) {
+  const checkResult = await bcrypt.compare(password, this.hashedPassword);
+  return checkResult;
+};
+userSchema.methods.serialize = function () {
+  const userData: IUser = this.toJSON();
+  delete userData.hashedPassword;
+  return userData;
 };
 
 // 정적 메서드
-userSchema.statics.findByUsername = function (username: string) {
+userSchema.statics['findByUsername'] = function (username: string) {
   return this.findOne({ username });
 };
 
-const User = mongoose.model('User', userSchema);
-
+const User = mongoose.model<IUserDocument, IUserModel>('User', userSchema);
 export default User;
